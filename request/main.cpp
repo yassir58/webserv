ea0f2f7f4b9edcec8726cd6e3f3fa9ba2cd6073c
Ma3ert <yahyaitiazza15@gmail.com>
2022-12-19 18:38:50 +0100

    parse of the first line is done start with header fields

--- request/main.cpp
+++ request/main.cpp
@@ -6,7 +6,7 @@
 /*   By: Ma3ert <yait-iaz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 15:35:19 by Ma3ert            #+#    #+#             */
-/*   Updated: 2022/12/16 14:31:37 by Ma3ert           ###   ########.fr       */
+/*   Updated: 2022/12/19 18:24:38 by Ma3ert           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,18 +63,25 @@ int checkRequestTarget(std::string target, t_start &line)
 
 int parseFirstLine(std::string line, std::string &method, std::string &target, std::string &version)
 {
-	size_t start = 0;
+	size_t start = line.find_first_not_of(' ', 0);
 	size_t pos = line.find(' ', start);
 	method = line.substr(start, pos);
-	
-	start = pos + 1;
+	if (method.empty())
+		return (1);
+
+	start = line.find_first_not_of(' ', pos);
 	pos = line.find(' ', start);
 	target = line.substr(start, pos - start);
-	
-	start = pos + 1;
+	if (target.empty())
+		return (2);
+
+	start = line.find_first_not_of(' ', pos);
 	pos = line.find(' ', start);
 	version = line.substr(start, pos);
-	return (1);
+	if (version.empty())
+		return (3);
+
+	return (0);
 }
 
 int checkVersion(std::string version, t_start &startLine)
@@ -94,8 +101,11 @@ int	treatFirstLine(std::string file, t_start &startLine)
 	std::string	target;
 	std::string version;
 
-	if (!parseFirstLine(file, method, target, version))
+	if (parseFirstLine(file, method, target, version))
+	{
+		std::cout << "error while parsing check the startup line\n";
 		return (0);
+	}
 	if (!checkMethod(method, startLine))
 		return (0);
 	if (!checkRequestTarget(target, startLine))
@@ -105,28 +115,42 @@ int	treatFirstLine(std::string file, t_start &startLine)
 	return (1);
 }
 
+int parseHeaderField(std::list<headerField> &list, std::string line)
+{
+	headerField	field;
+	size_t		start = 0;
+	size_t		pos = line.find(':', start);
+
+	field.key = line.substr(start, pos);
+	start = line.find_first_not_of(' ', pos + 1);
+	field.value = line.substr(start, std::string::npos);
+	list.push_back(field);
+	return (1);
+}
+
+int treatHeaderField(t_request newRequest, std::list<headerField> headerList)
+{
+}
+
 int main(int ac, char **av)
 {
 	std::ifstream fileRequest("request.txt");
 	std::string line;
-	std::string firstLine;
+	std::list<headerField> headerList;
 	t_request	newRequest;
 
 	if (!fileRequest.is_open())
 		return 1;
-	getline(fileRequest, firstLine);
-	if (!treatFirstLine(firstLine, newRequest.startLine))
+	getline(fileRequest, line);
+	if (!treatFirstLine(line, newRequest.startLine))
 		return 1;
-	std::cout << std::endl;
-	std::cout << "method: |" << newRequest.startLine.method << "|\n";
-	std::cout << "version: |" << newRequest.startLine.httpVersion << "|\n";
-	std::cout << "target: |" << newRequest.startLine.requestTraget << "|\n";
-	std::cout << "host: " << newRequest.startLine.hostName << "\n";
-	// while (line.empty())
-	// {
-	// 	getline(fileRequest, line);
-	// 	treatHeaderField();
-	// }
+	while (getline(fileRequest, line) && !line.empty())
+	{
+		if (!parseHeaderField(headerList, line))
+			return (0);
+		
+	}
+	treatHeaderField(newRequest, headerList);
 	// if (line.c_str() && newRequest.startLine.method == "POST")
 	// {
 	// 	std::ofstream body;
--- request/request.h
+++ request/request.h
@@ -6,12 +6,13 @@
 /*   By: Ma3ert <yait-iaz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 16:01:13 by Ma3ert            #+#    #+#             */
-/*   Updated: 2022/12/15 13:35:53 by Ma3ert           ###   ########.fr       */
+/*   Updated: 2022/12/19 18:27:01 by Ma3ert           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <fstream>
+#include <list>
 #include <iostream>
 #include <cstring>
 
@@ -23,9 +24,17 @@ typedef struct start
 	std::string	hostName;
 } t_start;
 
+typedef struct headerField
+{
+	std::string key;
+	std::string value;
+} headerField;
+
 typedef struct request
 {
 	t_start		startLine;
-	std::string	hostName;
-}t_request;
+	std::list<headerField>	generalField;
+	std::list<headerField>	requestField;
+	std::list<headerField>	entityField;
+} t_request;
 
