40d54ed403e4a5bc782d1e0008734739338abcad
Ma3ert <yahyaitiazza15@gmail.com>
2022-12-26 11:53:19 +0100

    remove unsed file

--- request/test.cpp
+++
@@ -1,8 +0,0 @@
-#include "request.h"
-
-int main()
-{
-	std::string sbardila = {"sbardila"};
-	std::cout << sbardila[sbardila.length() - 1] << std::endl;
-	std::cout << sbardila.substr(1, sbardila.length() - 1);
-}
