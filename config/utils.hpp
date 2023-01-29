e9c23af846d612878ed2323d13b92943b58c6426
lhaj-matich <choumouloussama@gmail.com>
2023-01-29 03:23:59 +0100

    Implemented the MiMe types list.

--- config/utils.hpp
+++ config/utils.hpp
@@ -2,6 +2,16 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
 #include <iostream>
+#include <exception>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <map>
+
+typedef std::map<std::string, std::string> mapContainer;
 
 const char * httpContext[] = {
         "send_file",
