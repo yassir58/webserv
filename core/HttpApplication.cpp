db2cd9996f12b96fa54e2edd4cdd33b494fa4b8b
lhaj-matich <choumouloussama@gmail.com>
2023-02-07 10:43:25 +0100

    Finished testing CGI.

--- core/HttpApplication.cpp
+++ core/HttpApplication.cpp
@@ -287,7 +287,6 @@ void HttpApplication::handleHttpResponse (int fd)
 		// }
 		if (request->getCGIStatus())
 		{
-			std::cout << "Executing CGI" << std::endl;
 			newCgi = new CGIHandler (request);
 			response = newCgi->execute();
 		}
