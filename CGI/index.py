7df273ba1388e521e6963f67c44264bf45ce5f58
Ma3ert <yahyaitiazza15@gmail.com>
2023-02-23 10:40:24 +0100

    Testing Python CGI.

--- CGI/index.py
+++ CGI/index.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 
-import cgi, cgitb
-cgitb.enable()
+# import cgi, cgitb
+# cgitb.enable()
 
-postData = cgi.FieldStorage()
+# postData = cgi.FieldStorage()
 
-print('Content-Type: text/html') # HTML is following
-print('');
+# print('Content-Type: text/html') # HTML is following
+# print('');
 
-username = postData["username"].value;
-password = postData["password"].value;
+# username = postData["username"].value;
+# password = postData["password"].value;
 
-print(username);
-print(password);
+# print(username);
+# print(password);
+
+print(tkhrib9a);
